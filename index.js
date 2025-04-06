6aecc0952031b4779f7fb150369039a9f353d661
Ashish Padhy <ashishpadhy1729@gmail.com>
2025-04-06 23:32:06 +0530

    chore: minor changes
    Signed-off-by: Ashish Padhy <ashishpadhy1729@gmail.com>

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import path from "path";
 
 const fileName = process.argv[2];
 
-const cwd = path.resolve(path.dirname(fileName));
-console.debug('CWD', cwd);
-
+// const cwd = path.resolve(path.dirname(fileName));
+// console.debug('CWD', cwd);
+let cwd = "action-test-bed/packages/eslint-config-custom";
 const pack = await readPackageUp({ cwd });
 console.debug('Package', pack);
