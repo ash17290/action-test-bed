7374bad7ade3909c4b0b45dc7d5943e04bb585f1
Ashish Padhy <ashishpadhy1729@gmail.com>
2025-04-06 19:30:55 +0530

    Update index.js

--- .github/workflows/changeset-utils/index.js
+++ .github/workflows/changeset-utils/index.js
@@ -1,8 +1,7 @@
 const getChangesetContents = async (pullRequest) => {
   const title = pullRequest.title;
   const releaseType = title.split(':')[0];
-  const timestamp = Date.now();
-  return `---\n'@asyncapi/cli': ${releaseType}\n---\n\n ${title}${timestamp}\n`;
+  return `---\n'@asyncapi/cli': ${releaseType}\n---\n\n ${title}\n`;
 };
 
 module.exports = {
